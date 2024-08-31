1f27c28a77e3547e237528e7867aa2387dec791b
avilac27122 <cvavila2009@gmail.com>
2024-08-31 11:00:55 -0400

    Hardware and Teleop for RI3W first iteration

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/RI3WHardware.java
@@ -0,0 +1,46 @@
+package org.firstinspires.ftc.teamcode.hardware;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+public class RI3WHardware {
+
+    public DcMotorEx frontLeft;
+    public DcMotorEx backLeft;
+    public DcMotorEx frontRight;
+    public DcMotorEx backRight;
+
+    public void init(HardwareMap hardwareMap, Telemetry telemetry){
+        frontLeft = hardwareMap.get(DcMotorEx.class, "frontLeft");
+        frontRight = hardwareMap.get(DcMotorEx.class, "frontRight");
+        backLeft = hardwareMap.get(DcMotorEx.class, "backLeft");
+        backRight = hardwareMap.get(DcMotorEx.class, "backRight");
+
+        frontRight.setDirection(DcMotorSimple.Direction.REVERSE);
+        backRight.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        frontLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        frontRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        frontLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        frontRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        frontLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        frontLeft.setPower(0);
+        frontRight.setPower(0);
+        backLeft.setPower(0);
+        backRight.setPower(0);
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/RI3WTeleop.java
@@ -0,0 +1,44 @@
+package org.firstinspires.ftc.teamcode.teleop;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.Utilities.ImprovedGamepad;
+import org.firstinspires.ftc.teamcode.hardware.RI3WHardware;
+
+@TeleOp(name = "RI3WTeleop")
+public class RI3WTeleop extends OpMode {
+
+    public RI3WHardware robot = new RI3WHardware();
+    double turningPower = 0;
+    public ImprovedGamepad gamepad;
+
+    @Override
+    public void init() {
+        gamepad = new ImprovedGamepad(gamepad1, new ElapsedTime(), "Gamepad");
+        robot.init(this.hardwareMap, telemetry);
+    }
+
+    @Override
+    public void loop() {
+        gamepad.update();
+
+        if (gamepad.right_trigger.getValue() > 0) {
+            turningPower = .3 * gamepad.right_trigger.getValue();
+        } else if (gamepad.left_trigger.getValue() > 0) {
+            turningPower = -.3 * gamepad.left_trigger.getValue();
+        } else {
+            turningPower = .75 * gamepad.right_stick.x.getValue();
+        }
+
+        double y = 1 * gamepad.left_stick.y.getValue();
+        double x = 1 * gamepad.left_stick.x.getValue();
+        double rx = turningPower;
+
+        robot.frontLeft.setPower(y + x + rx);
+        robot.frontRight.setPower(y - x - rx);
+        robot.backLeft.setPower(y - x + rx);
+        robot.backRight.setPower(y + x - rx);
+    }
+}
