a6a5903a94fa316a682fa3b2fc029b6305be228a
avilac27122 <cvavila2009@gmail.com>
2024-09-12 15:39:37 -0400

    Added skeleton of computerVisionProcessor if needed later

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/vision/computerVisionProcessor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/vision/computerVisionProcessor.java
@@ -1,4 +1,27 @@
 package org.firstinspires.ftc.teamcode.hardware.vision;
 
-public class computerVisionProcessor {
+import android.graphics.Canvas;
+
+import org.firstinspires.ftc.robotcore.internal.camera.calibration.CameraCalibration;
+import org.firstinspires.ftc.vision.VisionProcessor;
+import org.opencv.core.Mat;
+
+//Not needed yet. We can make when we decide if we need camera.
+
+public class computerVisionProcessor implements VisionProcessor {
+
+    @Override
+    public void init(int width, int height, CameraCalibration calibration) {
+
+    }
+
+    @Override
+    public Object processFrame(Mat frame, long captureTimeNanos) {
+        return null;
+    }
+
+    @Override
+    public void onDrawFrame(Canvas canvas, int onscreenWidth, int onscreenHeight, float scaleBmpPxToCanvasPx, float scaleCanvasDensity, Object userContext) {
+
+    }
 }
