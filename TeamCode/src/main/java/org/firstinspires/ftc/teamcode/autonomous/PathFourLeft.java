eb27bf3c3fea08a9e67195aafa0605fbf537ddc1
mcgeeo27445 <mcgeeo27445@student.cghsnc.org>
2024-10-03 15:12:40 -0400

    Edited PathFourLeft

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/PathFourLeft.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/PathFourLeft.java
@@ -1,44 +1,53 @@
 package org.firstinspires.ftc.teamcode.autonomous;
 
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
 @Autonomous (name = "PathFourLeft")
 public class PathFourLeft extends RI3WAbstractAutonomous {
     @Override
     public void runOpMode() throws InterruptedException {
+        // Initialize FTC Dashboard to show the telemetry
+        telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
+
         // Robot moves to cage, moves to sample, moves sample to area, and goes back to park
+
 
         robot.init(hardwareMap, telemetry);
         waitForStart();
 
         //Robot strafes left 22 in.
-        move(0,-22);
-        waitForContinue();
-        //Robot moves 45 in. forward
-        move(45,0);
-        waitForContinue();
-        //Robot strafes left 17 in.
-        move(0,-17);
-        waitForContinue();
+        move(0,-23);
+        //waitForContinue();
+        //Robot moves 46 in. forward
+        move(46,0);
+        //waitForContinue();
+        //Robot strafes left 15 in.
+        move(0,-14);
+        //waitForContinue();
+        // robot moves backward 20 inches
+        move(-26, 0);
+        //waitForContinue();
+        // robot moves to the left 3 inches - to move the block a little
+        move(0, -6);
+        //waitForContinue();
         //Robot moves backward 42 in.
         // TODO (stopped here)
-        move(-44, 0);
-        waitForContinue();
+        move(-18, 0);
+        //waitForContinue();
         //Robot strafes left 3 in.
         move(0,-3);
-        waitForContinue();
+        //waitForContinue();
         //Robot moves backward 12 in.
-        move(-12,0);
-        waitForContinue();
-        //Robot strafes right 10 in.
-        move(0,10);
-        waitForContinue();
-        //Robot moves 20 in. forward
         move(20,0);
-        waitForContinue();
+        //waitForContinue();
+        //Robot moves 20 in. backward
+        //move(14,0);
+        //waitForContinue();
         //Robot strafes right 92 in.
-        move(0,92);
-        waitForContinue();
+        move(0,102);
+        //waitForContinue();
         //Robot moves 20 in. backward
         move(-20,0);
     }
