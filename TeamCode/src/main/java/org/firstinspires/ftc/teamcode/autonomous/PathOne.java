5b6202c709deb6ca4cb1ccf4de9ae8a1d4f4648e
landrya27384 <landrya27384@student.cghsnc.org>
2024-10-31 16:00:50 -0400

    Consolidated PathOneRight and PathOneLeft, deleted PathOneRight, and renamed PathOneLeft to PathOne

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/PathOne.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/PathOne.java
@@ -2,25 +2,39 @@ package org.firstinspires.ftc.teamcode.autonomous;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
-@Autonomous(name = "PathOneRight")
-public class PathOneRight extends RI3WAbstractAutonomous {
+@Autonomous(name = "PathOne")
+public class PathOne extends RI3WAbstractAutonomous {
 
     @Override
     public void runOpMode() throws InterruptedException {
+        // 
         robot.init(hardwareMap, telemetry);
         setUp();
         waitForStart();
-        //TODO get robot to stop in same place for both left and right.
+
         if (whereToStart == StartingPosition.LEFT){
             //Robot strafes 3 in. right
-            move(0,18);
-            move(20 - robot.clawOffset, 0);
+            move(0,3);
+            // move(0,18);
             // move forward 30" - clawOffset
-            move(20 - robot.clawOffset, 0);
+            move(30 - robot.clawOffset, 0);
+            //move(20 - robot.clawOffset, 0);
         } else if (whereToStart == StartingPosition.RIGHT){
             // move forward 30" - clawOffset
-            move(0, -13);
-            move(20 - robot.clawOffset, 0);
+            move(30 - robot.clawOffset, 0);
+            //move(0, -13);
+            //move(20 - robot.clawOffset, 0);
+        }
+
+        try {
+            parsePath(
+                    "Strafe right 3 inches\n" +
+                    "Move forward 30 inches clawOffset\n" +
+                    "Strafe right 64 inches\n" +
+                    "Move back 24 inches\n"
+            );
+        } catch (Exception e) {
+            throw new RuntimeException(e);
         }
 
         // extends slides to drop height
@@ -35,6 +49,12 @@ public class PathOneRight extends RI3WAbstractAutonomous {
         // move right 40"
         //move(0, 52);
         // TODO park in corner and edge
+
+        // strafe right 64 in.
+        move(0,64);
+        // move back 24 in.
+        move(-24,0);
+
         if (whereToPark == ParkPosition.CORNER){
             //
         } else if (whereToPark == ParkPosition.EDGE){
