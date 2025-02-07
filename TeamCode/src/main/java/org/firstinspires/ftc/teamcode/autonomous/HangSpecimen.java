96ce4343fb90eccfb969ec1cae29d9e2e49f5b57
avilac27122 <cvavila2009@gmail.com>
2025-02-07 16:34:24 -0500

    Finalized Right autonomous restored RI6W autonomous for left path. Changed tick value for highChamber preset.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/HangSpecimen.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/HangSpecimen.java
@@ -17,26 +17,15 @@ public class HangSpecimen extends RI3WAbstractAutonomous {
 
         if (AutoConfig.getInstance().whereToStart == StartingPosition.LEFT){
             parsePath(
-                    "Claw close\n"+
-                            "Lift base\n"+
-                            "Move forward 25 inches\n"+
-                            "Lift highChamber\n"+
-                            "Slide extend 0.5\n"+
-                            "Move forward 1.5 inches\n"+
-                            "Lower 580\n"+
-                            "Slide retract 0.2\n"+
-                            "Move backward 3 inches\n"+
-                            "Claw open\n"+
-                            "Lift base\n"+
-                            // Specimen 1 is hung
-                            "Strafe right 31 inches\n"+
-                            "Move forward 22 inches\n"+
-                            "Strafe right 11 inches\n"+
-                            "Move backward 44 inches\n"+
-                            "Move forward 46 inches\n" +
-                            "TurnA counterclockwise 90 degrees\n" + //positive or negative 90?
-                            "Move backward 12 inches\n" +
-                            "Lift highChamber\n"
+                    "Strafe left 29 inches\n" +
+                            "Move forward 52 inches\n" +
+                            "Strafe left 6 inches\n" +
+                            "Move backwards 47 inches\n" +
+                            "Move forward 49 inches\n" +
+                            "Strafe left 8 inches\n"+
+                            "Move backwards 47 inches\n"+
+                            "Move forward 50 inches\n"+
+                            "Strafe right 12 inches\n"
             );
         } else if (AutoConfig.getInstance().whereToStart == StartingPosition.RIGHT) {
             parsePath(
@@ -45,38 +34,41 @@ public class HangSpecimen extends RI3WAbstractAutonomous {
                             "Move forward 25 inches\n"+
                             "Lift highChamber\n"+
                             "Slide extend 0.5\n"+
-                            "Move forward 1.5 inches\n"+
+                            "Move forward 2.5 inches\n"+
                             "Lower 580\n"+
                             "Slide retract 0.2\n"+
-                            "Move backward 3 inches\n"+
+                            "Move backward 4 inches\n"+
                             "Claw open\n"+
                             "Lift base\n"+
                             // Specimen 1 is hung
                             "Strafe right 31 inches\n"+
                             "Move forward 22 inches\n"+
                             "Strafe right 11 inches\n"+
-                            "Move backward 44 inches\n"+
+                            "Move backward 44 inches\n"
                             // Specimen 2 has been pushed into area
-                            "Move forward 16 inches\n"+
-                            "TurnA clockwise 180 degrees\n"+
-                            //"Wait 1000 ms\n" +
-                            //"Move forward 14 inches\n"+
-                            "Lift ground\n"+
-                            "MoveToWithin 5 inches\n"+
-                            "Claw close\n" +
-                            "Lift base\n"+
-                            "Strafe right 50 inches\n"+
-                            "TurnA clockwise 0 degrees\n"+
-                            "Move forward 22 inches\n"+
-                            "Lift highChamber\n"+
-                            "Slide extend 0.5\n"+
-                            "Move forward 1.5 inches\n"+
-                            "Lower 580\n"+
-                            "Slide retract 0.2\n"+
-                            "Move backward 3 inches\n"+
-                            "Claw open\n"+
-                            "Lift base\n"
-
+//                            "Move forward 16 inches\n"+
+//                            "TurnA clockwise 180 degrees\n"+
+//                            "Wait 1000 ms\n" +
+//                            "Move forward 14 inches\n"+
+//                            "Lift ground\n"+
+//                            "Claw close\n" +
+//                            "Lift base\n"+
+//                            "Strafe right 50 inches\n"+
+//                            "TurnA clockwise 0 degrees\n"+
+//                            "Move forward 23 inches\n"+
+//                            "Lift highChamber\n"+
+//                            "Slide extend 0.5\n"+
+//                            "Move forward 1.5 inches\n"+
+//                            "Lower 580\n"+
+//                            "Slide retract 0.2\n"+
+//                            "Move backward 3 inches\n"+
+//                            "Claw open\n"+
+//                            "Lift base\n"+
+//                            "Strafe left 45 inches\n"+
+//                            "Move forward 20 inches\n"+
+//                            "TurnR clockwise 90 degrees\n"+
+//                            "Move backward 12 inches\n"+
+//                            "Lift highChamber\n"
             );
         }
 
